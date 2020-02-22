2023c3fc5b14c80a42be7a923db7b32b2800f77b
kiran ram <kram@lions.molloy.edu>
2020-02-22 18:21:55 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,4 +1,11 @@
 require 'pry'
+def my_all?(collection)
+  i = 0
+  while i < collection.length
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  end
+end
+
 
 def my_all?(collection)
 all_odd = [1,3].all? do |number|
